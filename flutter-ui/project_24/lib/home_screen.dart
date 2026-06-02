cebc0dc9270241125b1a17ae705404f0aedcae4a
zainsafi <zainulislamk456@gmail.com>
2026-06-02 08:24:17 +0500

    implement ColorScheme and ThemeData theming

---
+++ flutter-ui/project_24/lib/home_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  final VoidCallback toggleTheme;
+  const HomeScreen({super.key, required this.toggleTheme});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme kColors = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        // you can also change the color but let it decided by the colorscheme
+        // backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          'Home screen',
+          // style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+        ),
+        iconTheme: IconThemeData(
+          // color: Theme.of(context).colorScheme.onPrimary,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          spacing: 10,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 100,
+              color: kColors.primary,
+              child: Center(
+                child: Text(
+                  'Primary',
+                  style: TextStyle(color: kColors.onPrimary),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 100,
+              color: kColors.secondary,
+              child: Center(
+                child: Text(
+                  'Primary',
+                  style: TextStyle(color: kColors.onSecondary),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 100,
+              color: kColors.primaryContainer,
+              child: Center(
+                child: Text(
+                  'Primary',
+                  style: TextStyle(color: kColors.onPrimaryContainer),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 100,
+              color: kColors.tertiary,
+              child: Center(
+                child: Text(
+                  'Primary',
+                  style: TextStyle(color: kColors.onTertiary),
+                ),
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Button')),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter Your Name",
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          widget.toggleTheme();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ flutter-ui/project_24/lib/main.dart
@@ -0,0 +1,68 @@
+// Flutter ColorScheme and colorScheme.fromSeed()Learning Project
+//  ColorScheme provides a set of semantic color roles for an
+//   application instead of using individual colors everywhere.
+
+// Material widgets: These generally have built-in Material theming e.g
+// Card, ElevatedButton, FilledButton, OutlinedButton, TextButton, AppBar,
+// NavigationBar, Dialog, Checkbox, Radio, Switch, Slider, TextField.
+// They can use your ThemeData and ColorScheme automatically.
+// you can also change or set it manually
+
+// Generic widgets: These don't automatically decide which ColorScheme
+// role you want. e.g
+// Container, SizedBox, Padding, Row, Column, Center, Stack, Expanded
+
+// Not focusing on design
+
+import 'package:flutter/material.dart';
+import 'package:project_24/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
+
+  void toggleTheme() {
+    setState(() {
+      isDarkMode = !isDarkMode;
+    });
+  }
+
+  ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.teal,
+      brightness: Brightness.light,
+    ),
+  );
+
+  ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.teal,
+      brightness: Brightness.dark,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+
+      darkTheme: darkTheme,
+
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: HomeScreen(toggleTheme: toggleTheme),
+    );
+  }
+}
